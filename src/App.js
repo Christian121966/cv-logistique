fcc056adce70668794a9863e453eca0ce6800d0d
Christian121966 <boissychristian@gmail.com>
2024-06-21 16:37:44 +0200

    Linked CSS files and ensured styling is applied

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Section from './components/Section';
-import ProfilePicture from './components/ProfilePicture';
+//import ProfilePicture from './components/ProfilePicture';
 import cvData from './data/cvData';
-import profileImage from './path/to/photo.jpg'; // Assurez-vous de mettre la photo dans le bon chemin
+//import profileImage from './path/to/photo.jpg'; // Assurez-vous de mettre la photo dans le bon chemin
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ProfilePicture src={profileImage} />
+      {/*<ProfilePicture src={profileImage} />*/}
       <main>
         <Section title="Formation">
           {cvData.formation.map((item, index) => (
