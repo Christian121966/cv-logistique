62b9f3b084751e12fc668e51256974238454c365
Christian121966 <boissychristian@gmail.com>
2024-06-27 02:21:49 +0200

    Avec des cards

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
 import React from 'react';
 import './App.css';
-//import Header from './components/Header';
 import Footer from './components/Footer';
 import Section from './components/Section';
 import Sidebar from './components/Sidebar';
-//import ProfilePicture from './components/ProfilePicture';
 import cvData from './data/cvData';
-//import profileImage from './path/to/photo.jpg'; // Assurez-vous de mettre la photo dans le bon chemin
 
 function App() {
   return (
     <div className="App">
-      {/*<Header />*/}
-      {/*<ProfilePicture src={profileImage} />*/}
       <div className="container">
-      
         <Sidebar />
-      
-      <main>
-        <Section title="Formation">
-          {cvData.formation.map((item, index) => (
-            <div key={index}>
-              <strong>{item.date}</strong>: {item.title} - {item.details}
-            </div>
-          ))}
-        </Section>
-        <Section title="Compétences Techniques">
-          <h3>Développement Web</h3>
-          <ul>
-            {cvData.competences.developpementWeb.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
-          <h3>Logistique</h3>
-          <ul>
-            {cvData.competences.logistique.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
-        </Section>
-        <Section title="Expériences Professionnelles">
-          {cvData.experiences.map((item, index) => (
-            <div key={index}>
-              <strong>{item.date}</strong>: {item.title} - {item.details}
-            </div>
-          ))}
-        </Section>
-      </main>
+        <main>
+          <div className="card">
+            <Section title="Formation">
+              {cvData.formation.map((item, index) => (
+                <div key={index}>
+                  <strong>{item.date}</strong>: {item.title} - {item.details}
+                </div>
+              ))}
+            </Section>
+          </div>
+          <div className="card">
+            <Section title="Compétences Techniques">
+              <h3>Développement Web</h3>
+              <ul>
+                {cvData.competences.developpementWeb.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+              <h3>Logistique</h3>
+              <ul>
+                {cvData.competences.logistique.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </Section>
+          </div>
+          <div className="card">
+            <Section title="Expériences Professionnelles">
+              {cvData.experiences.map((item, index) => (
+                <div key={index}>
+                  <strong>{item.date}</strong>: {item.title} - {item.details}
+                </div>
+              ))}
+            </Section>
+          </div>
+        </main>
       </div>
       <Footer />
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Sidebar = () => {
   return (
     <aside className="sidebar">
+      <img src="/images/Chris.png" alt="Chris" className="profile-photo" />
       <h1>Christian Boissy</h1>
       <address>
         8, rue du Moulin, 78130 Les Mureaux<br />
@@ -15,27 +16,52 @@ const Sidebar = () => {
       </address>
       <h2>Compétences professionnelles et linguistiques</h2>
       <ul>
-        <li>Compétence 1</li>
-        <li>Compétence 2</li>
-        <li>Compétence 3</li>
-        <li>Compétence 4</li>
-        <li>Compétence 5</li>
+        <li>
+          <h3>Communication multilingue</h3> 
+          <span>Maîtrise du français, de l'anglais et de l'espagnol, avec expérience en traduction technique et en communication interculturelle.</span>
+          </li>
       </ul>
       <h2>Logiciel et environnements</h2>
       <ul>
-        <li>Compétence 1</li>
-        <li>Compétence 2</li>
-        <li>Compétence 3</li>
-        <li>Compétence 4</li>
-        <li>Compétence 5</li>
+        <li>
+          <h3>Microsoft Office Suite</h3>
+          <span>Utilisation avancée d'Excel, PowerPoint, et Word pour la gestion de données, la présentation d'informations et la documentation.</span>
+        </li>
+        <li>
+          <h3>Environnements de développement</h3> 
+          <span>Maîtrise de Visual Studio Code pour le développement de logiciels.</span>
+          </li>
+        <li>
+          <h3>Systèmes d'exploitation</h3>
+          <span>Expérience avec Windows, macOS, et Linux pour le déploiement d'applications et la gestion de serveurs.</span>
+          </li>
+        <li>
+          <h3>Outils de versioning</h3>
+          <span>Compétence dans l'utilisation de Git pour le contrôle de version et la collaboration en équipe.</span>
+        </li>
       </ul>
       <h2>Qualités humaines</h2>
       <ul>
-        <li>Compétence 1</li>
-        <li>Compétence 2</li>
-        <li>Compétence 3</li>
-        <li>Compétence 4</li>
-        <li>Compétence 5</li>
+        <li>
+          <h3>Capacité d'adaptation</h3> 
+          <span>Aptitude à s'adapter rapidement aux nouvelles technologies et environnements de travail.</span>
+        </li>
+        <li>
+          <h3>Esprit d'équipe</h3> 
+          <span>Collaboration efficace dans des équipes multidisciplinaires et multiculturelles.</span>
+        </li>
+        <li>
+          <h3>Leadership</h3> 
+          <span>Expérience en tant que chef de projet, guidant les équipes vers la réalisation des objectifs.</span>
+        </li>
+        <li>
+          <h3>Résolution de problèmes</h3> 
+          <span>Compétence avérée dans l'identification et la résolution de problèmes complexes de manière créative et efficace.</span>
+        </li>
+        <li>
+          <h3>Empathie</h3> 
+          <span>Forte capacité à comprendre et à répondre aux besoins des clients et des collègues.</span>
+        </li>
       </ul>
     </aside>
   );
