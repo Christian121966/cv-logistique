6be0d0f8f2d69dbb851ef18538b4e6f1ab440656
Christian121966 <boissychristian@gmail.com>
2024-08-14 10:55:17 +0200

    Updates

---
+++ precache-manifest.4f04b1fce5b4ec015cfa1ca059f00852.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d3aee82887f456a16997e913899f2f",
+    "url": "/cv-logistique/index.html"
+  },
+  {
+    "revision": "7016a24f076a5a7bd430",
+    "url": "/cv-logistique/static/css/main.b71aad9c.chunk.css"
+  },
+  {
+    "revision": "3192a45022ba1bfdc67e",
+    "url": "/cv-logistique/static/js/2.0efa0e54.chunk.js"
+  },
+  {
+    "revision": "7016a24f076a5a7bd430",
+    "url": "/cv-logistique/static/js/main.235a7e1a.chunk.js"
+  },
+  {
+    "revision": "7b652b15e206ef7ae49b",
+    "url": "/cv-logistique/static/js/runtime~main.801614fd.js"
+  }
+]);
